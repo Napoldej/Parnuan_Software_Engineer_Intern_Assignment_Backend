e4fef740acf953a1b9ef0ac01ae6fcce673272d9
Napoldej Passornratchakul <napoldej.p@ku.th>
2025-12-15 13:57:11 +0700

    Add UserRepository class for user management functionality

---
+++ src/repository/userRepository.ts
@@ -0,0 +1,28 @@
+import type { PrismaClient } from "@prisma/client";
+import PrismaSingleTon from "../helper/prismaSingleTon.js";
+import type { createUserType } from "../types/usertype.js";
+
+class UserRepository{
+    private prisma: PrismaClient;
+
+    constructor(){
+        this.prisma = PrismaSingleTon.getInstance();
+    }
+
+    async createUser(input: createUserType){
+        try{
+            const user = await this.prisma.user.create({
+                data:{
+                    email: input.email,
+                    name: input.name
+                }
+            })
+        }catch(error){
+            console.log("Error creating user:", error);
+            throw error;
+        }
+    }
+
+}
+
+export default UserRepository
