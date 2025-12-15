7611a8ec616add4580b6468c3ff68c1f0b9cb9b9
Napoldej Passornratchakul <napoldej.p@ku.th>
2025-12-15 14:52:20 +0700

    Refactor transaction management: update add_transaction method and implement delete, edit, and recoverable transaction methods

--- src/repository/transactionRepository.ts
+++ src/repository/transactionRepository.ts
@@ -1,5 +1,6 @@
 import type { PrismaClient, TransactionType } from "@prisma/client";
 import PrismaSingleTon from "../helper/prismaSingleTon.js";
+import type { createTransactionType, editTransactionType } from "../types/transactionType.js";
 
 class TransactionRepository{
     private prisma: PrismaClient
@@ -8,14 +9,14 @@ class TransactionRepository{
         this.prisma = PrismaSingleTon.getInstance();
     }
 
-    async add_transaction(user_id: string, amount: number, title: string, type: string){
+    async add_transaction(user_id: string, input: createTransactionType){
         try{
             const transaction = await this.prisma.transaction.create({
             data:{
-                title: title,
+                title: input.title,
                 user_id: user_id,
-                amount: amount,
-                type: type as TransactionType
+                amount: input.amount,
+                type: input.type as TransactionType
                 }
             })
             return transaction
@@ -70,5 +71,62 @@ class TransactionRepository{
                 throw error;
             }
         }
+    
+        async delete_transaction(transaction_id: string){
+            try{
+                const deleted_transaction = await this.prisma.transaction.update({
+                    where: {
+                        id: transaction_id,
+                        is_deleted: false
+                    },
+                    data: {
+                        is_deleted: true,
+                        deleted_at: new Date()
+                    }
+                    })
+                    return deleted_transaction
+                } catch(error){
+                    console.log("Error deleting transaction:", error);
+                    throw error;
+                    }
+                }
+        
+        async edit_transaction(transaction_id: string, input: editTransactionType){
+            try{
+                const updated_transaction = await this.prisma.transaction.update({
+                    where: {
+                        id: transaction_id,
+                        is_deleted: false
+                    },
+                    data: {
+                        ...input,
+                        type: input.type as TransactionType,
+                        updated_at: new Date()
+                    }
+                })
+                return updated_transaction
+            }catch(error){
+                console.log("Error editing transaction:", error);
+                throw error;
+            }
+        }
 
-}
+        async recoverable_transaction(transaction_id: string){
+            try{
+                const recovered_transaction = await this.prisma.transaction.update({
+                    where:{
+                        id: transaction_id,
+                        is_deleted: true
+                    },
+                    data:{
+                        is_deleted: false
+                    }
+                })
+                return recovered_transaction
+            } catch(error){
+                console.log("Error recovering transaction:", error);
+                throw error
+            }
+        }
+    }
+
