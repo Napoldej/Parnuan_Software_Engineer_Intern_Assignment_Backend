c8670ffad89d37dc8e0fbc78290d2ad971c7e5e7
Napoldej Passornratchakul <napoldej.p@ku.th>
2025-12-15 14:01:04 +0700

    Add UserController class for handle user creation endpoint

---
+++ src/controller/userController.ts
@@ -0,0 +1,20 @@
+import UserService from "../service/userService.js";
+import type { Request, Response } from "express";
+
+class UserController {
+    private userService: UserService
+
+    constructor(){
+        this.userService = new UserService();
+    }
+
+    async createUser(req: Request, res: Response) {
+        try {
+            const result = await this.userService.createUser(req.body);
+            res.status(201).json(result);
+        } catch (error) {
+            res.status(500).json({ error: "Failed to create user" });
+        }
+    }
+
+}
