fe323b0f89bdec083fde1ab85d019e192456795e
Napoldej Passornratchakul <napoldej.p@ku.th>
2025-12-15 14:08:40 +0700

    Initialize Express application and add nodemon for development

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import  express from "express";
+import userRouter from "./router/userRouter.js";
+
+class MainApp{
+    public app: express.Application;
+    private port: string | number
+
+    constructor(){
+        this.app = express();
+        this.app.use(express.json());
+        this.port = process.env.PORT || 3000;
+    }
+
+    public start(){
+        this.listen();
+        this.app.use(userRouter)
+        
+    }
+
+    public listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        })
+    }
+}
+
+const mainApp = new MainApp();
+mainApp.start();
